7435418cacee4c0c91067092dcdca27c3915fca0
Katarína Baďurová <ktbadurova@gmail.com>
2019-05-28 16:11:40 +0200

    refaktorizacia

--- src/test/java/tests/CalculatorTest.java
+++ src/test/java/tests/CalculatorTest.java
@@ -5,6 +5,9 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 
 public class CalculatorTest extends TestBase {
@@ -16,38 +19,74 @@ public class CalculatorTest extends TestBase {
 
     @Test
     public void enterNumbersAndClickOnSumButton (){
-        driver.findElement(By.id("firstInput")).sendKeys("4");
-        driver.findElement(By.id("secondInput")).sendKeys("3");
-        driver.findElement(By.id("count")).click();
-        Assert.assertEquals("7", driver.findElement(By.id("result")).getText());
+        enterFirstInput();
+        enterSecondInput();
+        sumNumbers();
+        Assert.assertEquals("7", getResults());
     }
 
     @Test
     public void enterNumbersAndClickOnDeductButton(){
-        driver.findElement(By.id("firstInput")).sendKeys("4");
-        driver.findElement(By.id("secondInput")).sendKeys("3");
-        driver.findElement(By.id("deduct")).click();
-        Assert.assertEquals("1",driver.findElement(By.id("result")).getText());
+        enterFirstInput();
+        enterSecondInput();
+        deductNumbers();
+        Assert.assertEquals("1",getResults());
     }
 
     @Test
     public void itShouldResetCalculator(){
-        driver.findElement(By.id("firstInput")).sendKeys("4");
-        driver.findElement(By.id("secondInput")).sendKeys("3");
-        driver.findElement(By.id("count")).click();
-        driver.findElement(By.id("reset")).click();
+        enterFirstInput();
+        enterSecondInput();
+        sumNumbers();
+        resetCalculator();
         Assert.assertTrue(driver.findElement(By.id("firstInput")).getAttribute("value").isEmpty());
         Assert.assertTrue(driver.findElement(By.id("secondInput")).getAttribute("value").isEmpty());
     }
 
     @Test
     public void itShouldDisplayLastCalculations(){
-        driver.findElement(By.id("firstInput")).sendKeys("4");
-        driver.findElement(By.id("secondInput")).sendKeys("3");
-        driver.findElement(By.id("count")).click();
-        Assert.assertEquals("4 + 3 = 7", driver.findElement(By.cssSelector("ul.latest-results li")).getText());
+        enterFirstInput();
+        enterSecondInput();
+        sumNumbers();
+        Assert.assertEquals("4 + 3 = 7", getLastCalculation().getText());
         Assert.assertEquals(1, driver.findElement(By.cssSelector("ul.latest-results li")).getText());
     }
+
+    private void enterFirstInput(){
+        driver.findElement(By.id("firstInput")).sendKeys("4");
+    }
+
+    private void enterSecondInput(){
+        driver.findElement(By.id("secondInput")).sendKeys("3");
+    }
+
+    private void sumNumbers(){
+        driver.findElement(By.id("count")).click();
+    }
+
+    private void deductNumbers(){
+        driver.findElement(By.id("deduct")).click();
+    }
+
+    private String getResults(){
+        return driver.findElement(By.id("result")).getText();
+    }
+
+    private void resetCalculator(){
+        driver.findElement(By.id("reset")).click();
+    }
+
+    private WebElement getLastCalculation(){
+        return driver.findElement(By.cssSelector("ul.latest-results li"));
+    }
+
+    //private List<WebElement> getLastCalculations(){
+        //return driver.findElement(By.cssSelector("ul.latest-results li"));
+    //}
+
+
+
+
 
 
 }
