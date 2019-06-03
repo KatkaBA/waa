fbb0588e65d9cadd700b52da48e3f1318296c190
Katarína Baďurová <ktbadurova@gmail.com>
2019-06-03 19:03:05 +0200

    refaktorizacia + pridanie FindBy

--- src/test/java/pages/SavingsCalculatorPage.java
+++ src/test/java/pages/SavingsCalculatorPage.java
@@ -2,26 +2,56 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 public class SavingsCalculatorPage {
-
-
     WebDriver pageDriver;
+
+    @FindBy(id = "fundSelect")
+    private WebElement fundSelect;
+
+    @FindBy(id = "oneTimeInvestmentInput")
+    private WebElement investmentInput;
+
+    @FindBy(id = "yearsInput")
+    private WebElement periodInput;
+
+    @FindBy(id = "emailInput")
+    private WebElement emailInput;
+
+    @FindBy(css = "button.btn-block")
+    private WebElement applyButton;
+
     public SavingsCalculatorPage(WebDriver driver) {
         this.pageDriver = driver;
         PageFactory.initElements(pageDriver, this);
     }
 
-
     public void addInvestment(String investment){
-        pageDriver.findElement(By.id("oneTimeInvestmentInput")).sendKeys(investment);
+        investmentInput.sendKeys(investment);
     }
 
     public void addPeriod(String period) {
-        pageDriver.findElement(By.id("yearsInput")).sendKeys(period);
+        periodInput.sendKeys(period);
     }
 
     public void addEmail(String email) {
-        pageDriver.findElement(By.id("emailInput")).sendKeys(email);}
+        emailInput.sendKeys(email);
+    }
+
+    public void selectFund(String fund) {
+        new Select(fundSelect).selectByVisibleText(fund);
+    }
+
+    public WebElement getApplyButton(){
+        return this.applyButton;
+    }
+
+    public void clickApplyButton() {
+        applyButton.click();
+    }
+
 }
--- src/test/java/tests/SavingsCalculatorTest.java
+++ src/test/java/tests/SavingsCalculatorTest.java
@@ -6,13 +6,15 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Select;
-import pages.CalculatorPage;
 import pages.SavingsCalculatorPage;
 
 public class SavingsCalculatorTest extends TestBase {
 
     private SavingsCalculatorPage  savingsCalculatorPage;
+
+    private String [] selectedFunds = {"Handelsbanken Aktiv 100", "Hoggwart's Fund",
+            "Fellowship investment group", "McDuck's safe", "Batman's Cave Development",
+            "Death Star real estate", "Tom & Jerry corp"};
 
     @Before
     public void openPage() {
@@ -21,44 +23,46 @@ public class SavingsCalculatorTest extends TestBase {
     }
 
     @Test
-    public void itShouldEnableButton (){
-        WebElement fundSelect = driver.findElement(By.id("fundSelect"));
-        new Select(fundSelect).selectByVisibleText("Handelsbanken Aktiv 100");
+    public void itShouldEnableButton () {
+        savingsCalculatorPage.selectFund("Handelsbanken Aktiv 100");
         savingsCalculatorPage.addInvestment("50");
         savingsCalculatorPage.addPeriod("3");
         savingsCalculatorPage.addEmail("vymyslena@test.sk");
-        Assert.assertTrue(driver.findElement(By.cssSelector("button.btn-block")).isDisplayed());
+
+        Assert.assertTrue(savingsCalculatorPage.getApplyButton().isDisplayed());
     }
 
     @Test
     public void itShouldVerifyIncomes () {
-        WebElement fundSelect = driver.findElement(By.id("fundSelect"));
-        new Select(fundSelect).selectByVisibleText("Handelsbanken Aktiv 100");
+        savingsCalculatorPage.selectFund("Handelsbanken Aktiv 100");
         savingsCalculatorPage.addInvestment("50");
         savingsCalculatorPage.addPeriod("3");
         savingsCalculatorPage.addEmail("vymyslena@test.sk");
-        Assert.assertFalse(driver.findElement(By.cssSelector("div.result div:first-child p")).getText().isEmpty());
-        Assert.assertFalse(driver.findElement(By.cssSelector("div.result div:nth-child(1) p")).getText().isEmpty());
+
+        WebElement totalIncome = driver.findElement(By.cssSelector("div.result div:first-child p"));
+        WebElement interestIncome = driver.findElement(By.cssSelector("div.result div:nth-child(1) p"));
+        Assert.assertFalse(totalIncome.getText().isEmpty());
+        Assert.assertFalse(interestIncome.getText().isEmpty());
     }
 
     @Test
     public void itShouldVerifyRisk () {
-        WebElement fundSelect = driver.findElement(By.id("fundSelect"));
-        new Select(fundSelect).selectByVisibleText("Handelsbanken Aktiv 100");
+        savingsCalculatorPage.selectFund("Handelsbanken Aktiv 100");
         savingsCalculatorPage.addInvestment("50");
         savingsCalculatorPage.addPeriod("3");
         savingsCalculatorPage.addEmail("vymyslena@test.sk");
+
         Assert.assertFalse(driver.findElement(By.cssSelector("div.result div:last-child p")).getText().isEmpty());
     }
 
     @Test
     public void itShouldApplyForSaving(){
-        WebElement fundSelect = driver.findElement(By.id("fundSelect"));
-        new Select(fundSelect).selectByVisibleText("Handelsbanken Aktiv 100");
+        savingsCalculatorPage.selectFund("Handelsbanken Aktiv 100");
         savingsCalculatorPage.addInvestment("50");
         savingsCalculatorPage.addPeriod("3");
         savingsCalculatorPage.addEmail("vymyslena@test.sk");
-        driver.findElement(By.cssSelector("button.btn-block")).click();
+        savingsCalculatorPage.clickApplyButton();
+
         Assert.assertTrue(driver.findElement(By.cssSelector("div.saving-detail")).isDisplayed());
         Assert.assertEquals("Handelsbanken Aktiv 100", driver.findElement(By.cssSelector("p.fund-description")).getText());
     }
